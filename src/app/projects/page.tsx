58fa77a3a8985a1141e371662653b997a93bca75
LakshyaSingh354 <lakshya.singh354@gmail.com>
2024-05-10 00:01:17 +0530

    completed header

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,12 @@
 import { Hero } from "../../../components/hero";
 import { TracingBeam } from "../../../components/ui/tracing-beam";
 
-
+export default function Project(){
+    return(
+        <div>
+            <h1>
+                Project Page
+            </h1>
+        </div>
+    )
+}
