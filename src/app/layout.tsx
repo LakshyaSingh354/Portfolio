1debf409ea58db3eaecec64f2b26293bfbad9dbb
LakshyaSingh354 <lakshya.singh354@gmail.com>
2024-05-17 15:56:13 +0530

    Add description to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lakshya Singh",
+  description: "Personal portfolio of Lakshya Singh. Lakshya Singh AI portfolio.",
   icons: { icon: "src/app/favicon.ico" },
 };
 
